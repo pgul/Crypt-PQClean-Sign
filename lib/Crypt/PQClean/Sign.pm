3e73ae361b55c250207a2b970671c9affdd27619
Pavel Gulchuk <gul@gul.kiev.ua>
2024-12-23 11:41:05 +0100

    Add more signature algorithms

--- lib/Crypt/PQClean/Sign.pm
+++ lib/Crypt/PQClean/Sign.pm
@@ -10,6 +10,36 @@ our @EXPORT_OK = qw(
     falcon512_keypair
     falcon512_sign
     falcon512_verify
+    falcon1024_keypair
+    falcon1024_sign
+    falcon1024_verify
+    mldsa44_keypair
+    mldsa44_sign
+    mldsa44_verify
+    mldsa65_keypair
+    mldsa65_sign
+    mldsa65_verify
+    mldsa87_keypair
+    mldsa87_sign
+    mldsa87_verify
+    sphincs_shake128f_keypair
+    sphincs_shake128f_sign
+    sphincs_shake128f_verify
+    sphincs_shake128s_keypair
+    sphincs_shake128s_sign
+    sphincs_shake128s_verify
+    sphincs_shake192f_keypair
+    sphincs_shake192f_sign
+    sphincs_shake192f_verify
+    sphincs_shake192s_keypair
+    sphincs_shake192s_sign
+    sphincs_shake192s_verify
+    sphincs_shake256f_keypair
+    sphincs_shake256f_sign
+    sphincs_shake256f_verify
+    sphincs_shake256s_keypair
+    sphincs_shake256s_sign
+    sphincs_shake256s_verify
 );
 
 require XSLoader;
@@ -37,7 +67,7 @@ Crypt::PQCrypt::Sign - Post-Quantum Cryptography with keypair
 
 =head1 DESCRIPTION
 
-  Provides an interface to the PQClean falcon-512 implementation.
+  Provides an interface to the PQClean signatures implementation.
 
 =head1 FUNCTIONS
 
@@ -48,6 +78,66 @@ Crypt::PQCrypt::Sign - Post-Quantum Cryptography with keypair
 =item B<falcon512_sign>
 
 =item B<falcon512_verify>
+
+=item B<falcon1024_keypair>
+
+=item B<falcon1024_sign>
+
+=item B<falcon1024_verify>
+
+=item B<mldsa44_keypair>
+
+=item B<mldsa44_sign>
+
+=item B<mldsa44_verify>
+
+=item B<mldsa65_keypair>
+
+=item B<mldsa65_sign>
+
+=item B<mldsa65_verify>
+
+=item B<mldsa87_keypair>
+
+=item B<mldsa87_sign>
+
+=item B<mldsa87_verify>
+
+=item B<sphincs_shake128f_keypair>
+
+=item B<sphincs_shake128f_sign>
+
+=item B<sphincs_shake128f_verify>
+
+=item B<sphincs_shake128s_keypair>
+
+=item B<sphincs_shake128s_sign>
+
+=item B<sphincs_shake128s_verify>
+
+=item B<sphincs_shake192f_keypair>
+
+=item B<sphincs_shake192f_sign>
+
+=item B<sphincs_shake192f_verify>
+
+=item B<sphincs_shake192s_keypair>
+
+=item B<sphincs_shake192s_sign>
+
+=item B<sphincs_shake192s_verify>
+
+=item B<sphincs_shake256f_keypair>
+
+=item B<sphincs_shake256f_sign>
+
+=item B<sphincs_shake256f_verify>
+
+=item B<sphincs_shake256s_keypair>
+
+=item B<sphincs_shake256s_sign>
+
+=item B<sphincs_shake256s_verify>
 
 =back
 
